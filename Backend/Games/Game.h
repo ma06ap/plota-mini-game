665fe7af15bd099bdc56a2337535099e7f8f47da
matin <matin.akbari06ap@gmail.com>
2026-02-13 19:25:14 +0330

    add history

--- Backend/Games/Game.h
+++ Backend/Games/Game.h
@@ -1,15 +1,21 @@
 #pragma once
 
 
+#include <filesystem>
+#include <format>
+
 #include "Board.h"
 #include "Turn.h"
+#include "Status.h"
 #include <string>
+#include <vector>
 
 
 class Game {
 protected:
      Board src;
      int id;
+     std::vector<Status> history;
      // Turn turn;
 public:
      virtual std::string getName() const = 0;
