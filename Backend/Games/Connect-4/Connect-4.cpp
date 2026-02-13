9f9b535b57bb4e7bb956a2f64cb9ba42ac80f213
matin <matin.akbari06ap@gmail.com>
2026-02-13 19:24:12 +0330

    add column option

--- Backend/Games/Connect-4/Connect-4.cpp
+++ Backend/Games/Connect-4/Connect-4.cpp
@@ -1,10 +1,24 @@
 #include "Connect-4.h"
 
 
-ConnectFour::ConnectFour() : Game() {
-
+ConnectFour::ConnectFour() : Game() , turn("Red","Yellow") {
+    src = Board(6, 7);
+    // history.push_back(Status(src, 0));
 }
 
 std::string ConnectFour::getName() const {
     return "Connect-4";
+}
+
+void ConnectFour::addColumn(int col) {
+    if (col < 0 || col >= src.getColumns()) {
+        throw std::out_of_range("Column index out of range");
+    }
+    Piece *tmp = new Piece(turn.get());
+    for (int i = src.getRows() - 1; i >= 0; --i) {
+        if (src.getPiece(i,col)==nullptr) {
+            continue;
+        }
+        src.Add(tmp, i + 1, col);
+    }
 }
--- Backend/Games/Connect-4/Connect-4.h
+++ Backend/Games/Connect-4/Connect-4.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "../Game.h"
+#include "../Turn.h"
 
 
 class ConnectFour : public Game {
+    Turn turn;
 public:
     ConnectFour();
     std::string getName() const override;
-
+    void addColumn(int col);
 
 
 };
